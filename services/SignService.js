e19249de0ff2a93cc726300805745abb9efa8a61
rm94231@fiap.com.br <rm94231@fiap.com.br>
2023-08-22 20:11:21 -0300

    user data on async storage

--- services/SignService.js
+++ services/SignService.js
@@ -1,26 +1,34 @@
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { FIREBASE_AUTH } from "../FirebaseConfig";
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useNavigation } from "@react-navigation/native";
 
 
 const auth = FIREBASE_AUTH;
 
-export const signInFirebase = async (email, senha) => {
+export const signInFirebase = async (emailTyped, senha) => {
   try{
-    const response = await signInWithEmailAndPassword(auth, email, senha);
+    const response = await signInWithEmailAndPassword(auth, emailTyped, senha);
+    const data = response.user
+    const {email, displayName} = data
+    const selectedValues = {email, displayName}
+    AsyncStorage.setItem("@USER_DATA", JSON.stringify(selectedValues))
   } catch(error) {
     console.error(error)
-    alert("lOGIN fALHOU")
+    alert("Login falhou")
   }
 }
 
-export const signUpFirebase = async (email, senha) => {
+export const signUpFirebase = async (emailTyped, senha, nome) => {
   try{
-    const response = await createUserWithEmailAndPassword(auth, email, senha);
+    const response = await createUserWithEmailAndPassword(auth, emailTyped, senha);
+    await updateProfile(auth.currentUser, {displayName: nome}).catch((e) => {console.log(e)})
+    const dataUser = response.user
+    let {email, displayName} = dataUser
+    let selectedValues = {email, displayName}  
+    AsyncStorage.setItem("@USER_DATA", JSON.stringify(selectedValues))
     alert("Cadastro criado com sucesso!")
   } catch(error) {
-    console.error(error)
-    alert("Cadastro falhou")
+      console.error(error)
+      alert("Cadastro falhou")
   }
 }
--- src/screens/Homescreen.js
+++ src/screens/Homescreen.js
@@ -1,8 +1,22 @@
+import React, {useEffect} from 'react'
 import { View, Text, Button } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { FIREBASE_AUTH } from '../../FirebaseConfig'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+
 
 export function Homescreen() {
+
+  const userData = async () => {
+    const value = await AsyncStorage.getItem("@USER_DATA")
+    console.log(JSON.parse(value))
+  }
+
+  useEffect(() => {
+    console.info(FIREBASE_AUTH.currentUser)
+  })
+
   return (
     <SafeAreaView>
       <Text>Homescreen</Text>
